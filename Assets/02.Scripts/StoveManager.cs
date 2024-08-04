da72ae34cca70e4745423222d07ac3e301df086a
KayeongAnna <yuminyang199145121@gmail.com>
2024-08-04 20:34:41 +0900

    feat stove script

---
+++ Assets/02.Scripts/StoveManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveManager : MonoBehaviour
+{
+
+    public GameObject fireEffect;
+    public GameObject smokeEffect;
+    public AudioSource fireSound;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
