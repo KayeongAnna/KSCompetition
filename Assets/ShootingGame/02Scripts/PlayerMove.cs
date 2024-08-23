af252ab8c60892376c9f09af4c67f4580bac3500
KayeongAnna <yuminyang199145121@gmail.com>
2024-08-23 13:05:47 +0900

    2D Testing Game

---
+++ Assets/ShootingGame/02Scripts/PlayerMove.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+    }
+}
